63122f852feef4ee70a09e5c5b0b2302a5533092
jifei <jifei@qingting.fm>
2021-11-12 18:03:56 +0800

    DeepFM model example

--- deeprs/models/rank/deepfm.py
+++ deeprs/models/rank/deepfm.py
@@ -3,69 +3,77 @@
  @author:jifei@outlook.com
  @date  :2021/11/3 21:28
 """
+from itertools import chain
 
 import tensorflow as tf
 from ...layers.fm import FM
 from ...layers.linear import Linear
 from ...layers.dnn import DNN
-from ...layers.Inputs import input_embedding_layer
-from tensorflow.keras.layers import Input, Flatten, Concatenate, Dense
-from tensorflow.keras.initializers import Zeros, glorot_normal
+from ...features.inputs import build_input_features, input_from_feature_columns
+from ...features.feature_column import DEFAULT_GROUP_NAME
+from tensorflow.keras.layers import Flatten, Concatenate, Dense
 
 from tensorflow.keras.models import Model
 
-def DeepFM(sparse_features,
-           sparse_feature_reindex_dict,
-           dense_features,
-           dnn_hidden_units=(256, 128, 64),
-           l2_reg_embedding=1e-5,
-           l2_reg_linear=1e-5,
-           l2_reg_dnn=0,
-           init_std=0.0001,
-           seed=1024,
-           bi_dropout=0.2,
-           dnn_dropout=0,
-           dnn_activation='relu',
-           embedding_dim=4):
-    # 1. Input & Embedding sparse features
-    sparse_input_layer_list = []
-    sparse_embedding_layer_list = []
-    linear_sparse_embedding_layer_list = []
-    for i in sparse_features:
-        shape = 1
-        name = i
-        vocabulary_size = len(sparse_feature_reindex_dict[i]) + 1
-        cur_sparse_feature_input_layer, cur_sparse_feature_embedding_layer, cur_linear_sparse_embedding_layer_list = input_embedding_layer(
-            shape=shape,
-            name=name,
-            vocabulary_size=vocabulary_size,
-            embedding_dim=embedding_dim,
-            l2_reg=l2_reg_embedding
-        )
 
-        sparse_input_layer_list.append(cur_sparse_feature_input_layer)
-        sparse_embedding_layer_list.append(cur_sparse_feature_embedding_layer)
-        linear_sparse_embedding_layer_list.append(cur_linear_sparse_embedding_layer_list)
+#
+# def combined_dnn_input(sparse_embedding_list, dense_value_list):
+#     if len(sparse_embedding_list) > 0 and len(dense_value_list) > 0:
+#         sparse_dnn_input = Flatten()(concat_func(sparse_embedding_list))
+#         dense_dnn_input = Flatten()(concat_func(dense_value_list))
+#         return concat_func([sparse_dnn_input, dense_dnn_input])
+#     elif len(sparse_embedding_list) > 0:
+#         return Flatten()(concat_func(sparse_embedding_list))
+#     elif len(dense_value_list) > 0:
+#         return Flatten()(concat_func(dense_value_list))
+#     else:
+#         raise NotImplementedError("dnn_feature_columns can not be empty list")
+#
 
-    # 2. Input dense features
-    dense_input_layer_list = []
-    for j in dense_features:
-        dense_input_layer_list.append(Input(shape=(1,), name=j))
+def DeepFM(linear_feature_columns, dnn_feature_columns, fm_group=(DEFAULT_GROUP_NAME,), dnn_hidden_units=(256, 128, 64),
+           l2_reg_linear=1e-5, l2_reg_embedding=1e-5, l2_reg_dnn=1e-5, seed=1024, dnn_dropout=0,
+           dnn_activation='relu', dnn_use_bn=False, task='binary'):
+    """Instantiates the DeepFM Network architecture.
 
-    # Linear part
-    sparse_linear_embedding_input = Concatenate(axis=-1)(linear_sparse_embedding_layer_list)
-    dense_linear_input = Concatenate(axis=-1)(dense_input_layer_list)
-    linear_logit = Linear()([sparse_linear_embedding_input, dense_linear_input])
+    :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
+    :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
+    :param fm_group: list, group_name of features that will be used to do feature interactions.
+    :param dnn_hidden_units: list,list of positive integer or empty list, the layer number and units in each layer of DNN
+    :param l2_reg_linear: float. L2 regularizer strength applied to linear part
+    :param l2_reg_embedding: float. L2 regularizer strength applied to embedding vector
+    :param l2_reg_dnn: float. L2 regularizer strength applied to DNN
+    :param seed: integer ,to use as random seed.
+    :param dnn_dropout: float in [0,1), the probability we will drop out a given DNN coordinate.
+    :param dnn_activation: Activation function to use in DNN
+    :param dnn_use_bn: bool. Whether use BatchNormalization before activation or not in DNN
+    :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+    :return: A Keras model instance.
+    """
 
-    # FM part
-    fm_logit = FM()(Concatenate(axis=1)(sparse_embedding_layer_list))
+    features = build_input_features(
+        linear_feature_columns + dnn_feature_columns)
 
-    # DNN part
+    group_embedding_dict, dense_value_list = input_from_feature_columns(features, dnn_feature_columns, l2_reg_embedding)
+    sparse_embedding_layer_list = list(chain.from_iterable(group_embedding_dict.values()))
+    fm_embedding_layer_list = []
+    for k, v in group_embedding_dict.items():
+        if k in fm_group:
+            fm_embedding_layer_list += v
+
+    dense_linear_input = Concatenate(axis=-1)(dense_value_list)
     sparse_linear_input = Concatenate(axis=-1)(sparse_embedding_layer_list)
-    dnn_input = Concatenate(axis=-1)([Flatten()(sparse_linear_input), dense_linear_input])
-    dnn_output = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, False, seed)(dnn_input)
-    dnn_logit = tf.keras.layers.Dense(1, use_bias=False, kernel_initializer=glorot_normal(seed=seed))(dnn_output)
 
-    # output
-    out = Dense(1, activation='sigmoid')(tf.keras.layers.add([linear_logit, fm_logit, dnn_logit]))
-    return Model(inputs=sparse_input_layer_list + dense_input_layer_list, outputs=out, name='DeepFM')
+    # linear part
+    linear_logit = Linear(l2_reg=l2_reg_linear)([sparse_linear_input, dense_linear_input])
+
+    # fm part
+    fm_logit = FM()(Concatenate(axis=1)(fm_embedding_layer_list))
+
+    # dnn part
+    dnn_input = Concatenate(axis=-1)([Flatten()(sparse_linear_input), dense_linear_input])
+    dnn_output = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(dnn_input)
+    dnn_logit = Dense(1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed=seed))(dnn_output)
+    # input and  output
+    inputs = list(features.values())
+    outputs = Dense(1, activation='sigmoid')(tf.keras.layers.add([linear_logit, fm_logit, dnn_logit]))
+    return Model(inputs=inputs, outputs=outputs)
---
+++ examples/criteo_deepfm.py
@@ -0,0 +1,86 @@
+# coding=utf-8
+"""
+ @author:jifei
+ @date  :2021/11/11 16:27
+"""
+import os
+import sys
+
+cur_path = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(cur_path)[0]
+sys.path.append(root_path)
+from deeprs.models.rank.deepfm import DeepFM
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from deeprs.features.feature_column import SparseFeature, DenseFeature
+from deeprs.features.inputs import get_feature_names
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+
+# 1.set hyper parameters
+test_size = 0.2
+validation_size = 0.1
+
+batch_size = 4096
+epochs = 5
+hidden_units = (256, 128, 64)
+dropout = 0.5
+learning_rate = 1e-3
+embedding_dim = 8
+l2_reg_dnn = 1e-5
+
+# 2.read data from csv
+names = ['label', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11',
+         'I12', 'I13', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11',
+         'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21', 'C22',
+         'C23', 'C24', 'C25', 'C26']
+# data = pd.read_csv("~/dataset/criteo/criteo_sampled_data.csv", sep='\t', header=None, names=names)
+data = pd.read_csv("~/dataset/criteo/criteo_sampled_data.csv")
+sparse_features = ['C' + str(i) for i in range(1, 27)]
+dense_features = ['I' + str(i) for i in range(1, 14)]
+data[sparse_features] = data[sparse_features].fillna('-1', )
+data[dense_features] = data[dense_features].fillna(0, )
+target = ['label']
+
+# 3.Label Encoding for sparse features,and do simple Transformation for dense features
+for feat in sparse_features:
+    lbe = LabelEncoder()
+    data[feat] = lbe.fit_transform(data[feat])
+mms = MinMaxScaler(feature_range=(0, 1))
+data[dense_features] = mms.fit_transform(data[dense_features])
+
+# 4.count unique features for each sparse field,and record dense feature field name
+fixlen_feature_columns = [SparseFeature(feature, vocabulary_size=data[feature].max() + 1, embedding_dim=embedding_dim)
+                          for i, feature in enumerate(sparse_features)] + \
+                         [DenseFeature(feature, 1, ) for feature in dense_features]
+
+dnn_feature_columns = fixlen_feature_columns
+linear_feature_columns = fixlen_feature_columns
+
+feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
+
+# 5.generate input data for model
+train, test = train_test_split(data, test_size=0.2, random_state=2021)
+train_model_input = {name: train[name] for name in feature_names}
+test_model_input = {name: test[name] for name in feature_names}
+
+# 6.define model
+mirrored_strategy = tf.distribute.MirroredStrategy()
+with mirrored_strategy.scope():
+    model = DeepFM(linear_feature_columns, dnn_feature_columns, dnn_hidden_units=hidden_units, dnn_dropout=dropout,
+                   l2_reg_dnn=l2_reg_dnn)
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss="binary_crossentropy",
+                  metrics=['binary_crossentropy', 'AUC'])
+
+# 7.print model summary and plot model
+print(model.summary())
+# from tensorflow.keras.utils import plot_model
+# plot_model(model, to_file='./deepfm_model.png', show_shapes=True)
+
+# 8.train and evaluate
+history = model.fit(train_model_input, train[target].values, batch_size=batch_size, epochs=epochs,
+                    validation_split=validation_size)
+
+result = model.evaluate(test_model_input, test[target].values, batch_size=batch_size)
+print(dict(zip(model.metrics_names, result)))
